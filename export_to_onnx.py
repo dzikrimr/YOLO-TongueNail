5cbff749f093d2d97220115d4bfe2c75916e192e
Dzvelocity <dzikrimurtadlo@gmail.com>
2025-10-07 13:33:42 +0700

    remove convert tflite and onnx

--- export_to_onnx.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load YOLOv8 trained model
-model = YOLO("runs/train/kuku/weights/best.pt")
-
-# Export ke ONNX
-model.export(format="onnx")  # hasil: runs/train/kuku/weights/best.onnx
-print("✅ Exported to ONNX successfully!")
